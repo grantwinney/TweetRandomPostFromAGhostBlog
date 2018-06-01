7addaa962b269bfffdbeb6c2cc7f034960b6e44a
Grant Winney <grantwinney@gmail.com>
2018-06-01 06:43:06 -0400

    tweet random post

---
+++ TweetRandomFeedItem/GhostEntities.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+
+namespace TweetRandomFeedItem
+{
+    public class PostResponse
+    {
+        public List<Post> Posts { get; set; }
+        public Meta Meta { get; set; }
+    }
+
+    public class Post
+    {
+        public string Id { get; set; }
+        public string Uuid { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Slug { get; set; }
+        public string MobileDoc { get; set; }
+        public string Html { get; set; }
+        public string PlainText { get; set; }
+        public string FeatureImage { get; set; }
+        public bool Featured { get; set; }
+        public bool Page { get; set; }
+        public string Status { get; set; }
+        public string Locale { get; set; }
+        public string Visibility { get; set; }
+        public string MetaTitle { get; set; }
+        public string MetaDescription { get; set; }
+        public string CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public string PublishedAt { get; set; }
+        public string PublishedBy { get; set; }
+        public string CustomExcerpt { get; set; }
+        public string CodeInjectionHead { get; set; }
+        public string CodeInjectionFoot { get; set; }
+        public string OgImage { get; set; }
+        public string OgTitle { get; set; }
+        public string OgDescription { get; set; }
+        public string TwitterImage { get; set; }
+        public string TwitterTitle { get; set; }
+        public string TwitterDescription { get; set; }
+        public string CustomTemplate { get; set; }
+        public List<Tag> Tags { get; set; }
+        public Tag PrimaryTag { get; set; }
+        public string Url { get; set; }
+        public string CommentId { get; set; }
+    }
+
+    public class Tag
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public string FeatureImage { get; set; }
+        public string Visibility { get; set; }
+        public string MetaTitle { get; set; }
+        public string MetaDescription { get; set; }
+        public string CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public string Parent { get; set; }
+    }
+
+    public class Meta
+    {
+        public Pagination Pagination { get; set; }
+    }
+
+    public class Pagination
+    {
+        public int Page { get; set; }
+        public int Limit { get; set; }
+        public int Pages { get; set; }
+        public int Total { get; set; }
+        public int Next { get; set; }
+        public int Prev { get; set; }
+    }
+
+    public class AuthToken
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public int ExpiresIn { get; set; }
+        public string TokenType { get; set; }
+    }
+}
---
+++ TweetRandomFeedItem/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using RestSharp;
+using Tweetinvi;
+
+namespace TweetRandomFeedItem
+{
+    class Program
+    {
+        const string API_URL = "/ghost/api/v0.1";
+        const string DEFAULT_MAX_TAG_COUNT = "3";
+        const string DEFAULT_POST_RETRIEVAL_LIMIT = "9999999";
+        const int TWITTER_URL_SIZE = 23;
+        const int MAX_TWEET_LENGTH = 280;
+        static string accessToken;
+
+        static Random rnd = new Random();
+
+        public static void Main()
+        {
+            var message = CreateMessage(GetPost(GetRandomPostId()));
+
+            SendTweet(message);
+
+            Console.WriteLine($"Tweeted message:\r\n\r\n{message}");
+        }
+
+        static string GetRandomPostId()
+        {
+            var ghostUri = Helper.GetEnv("GHOST_URI");
+            var postLimit = Helper.GetEnv("GHOST_POST_RETRIEVAL_LIMIT", DEFAULT_POST_RETRIEVAL_LIMIT);
+          
+            var client = new RestClient { BaseUrl = new Uri($"{ghostUri}{API_URL}") };
+
+            var requestAllPostIds = new RestRequest("posts", Method.GET);
+            AttachAuthToRequest(requestAllPostIds);
+
+            requestAllPostIds.AddQueryParameter("limit", postLimit);
+            requestAllPostIds.AddQueryParameter("fields", "id");
+
+            var posts = client.Execute<PostResponse>(requestAllPostIds).Data.Posts;
+           
+            return posts[rnd.Next(0, posts.Count - 1)].Id;
+        }
+
+        static Post GetPost(string postId)
+        {
+            var ghostUri = Helper.GetEnv("GHOST_URI");
+          
+            var client = new RestClient { BaseUrl = new Uri($"{ghostUri}{API_URL}") };
+
+            var request = new RestRequest($"posts/{postId}", Method.GET);
+            AttachAuthToRequest(request);
+
+            request.AddQueryParameter("include", "tags");
+
+            return client.Execute<PostResponse>(request).Data.Posts.Single();
+        }
+
+        static string CreateMessage(Post post)
+        {
+            var ghostUri = Helper.GetEnv("GHOST_URI");
+            var maxTagCount = Convert.ToInt32(Helper.GetEnv("TWITTER_MAX_TAG_COUNT", DEFAULT_MAX_TAG_COUNT));
+
+            var remainingSpaceForTags = MAX_TWEET_LENGTH - (post.Title.Length + 1 + TWITTER_URL_SIZE);
+
+            var pattern = new Regex("[- ]");
+            var tags = post.Tags.Take(maxTagCount).Select(t => $"#{pattern.Replace(t.Name, "").Replace("#", "sharp").Replace(".", "dot")}");
+          
+            var tagsFlat = "";
+            foreach (var t in tags)
+            {
+                if ($"{tagsFlat} {t}".Length > remainingSpaceForTags)
+                    break;
+                
+                tagsFlat += $" {t}";
+            }
+
+            return $"{post.Title}{tagsFlat}\r\n{ghostUri}{post.Url}";
+        }
+
+        static void SendTweet(string message)
+        {
+            var consumerKey = Helper.GetEnv("TWITTER_CONSUMER_KEY");
+            var consumerSecret = Helper.GetEnv("TWITTER_CONSUMER_SECRET");
+            var userAccessToken = Helper.GetEnv("TWITTER_USER_ACCESS_TOKEN");
+            var userAccessTokenSecret = Helper.GetEnv("TWITTER_USER_ACCESS_TOKEN_SECRET");
+
+            Auth.SetUserCredentials(consumerKey, consumerSecret, userAccessToken, userAccessTokenSecret);
+
+            Tweet.PublishTweet(message);
+        }
+
+        static void AttachAuthToRequest(RestRequest request)
+        {
+            var ghostUri = Helper.GetEnv("GHOST_URI");
+            var ghostClientId = Helper.GetEnv("GHOST_CLIENT_ID");
+            var ghostClientSecret = Helper.GetEnv("GHOST_CLIENT_SECRET");
+            var ghostUsername = Helper.GetEnv("GHOST_USERNAME");
+            var ghostPassword = Helper.GetEnv("GHOST_PASSWORD");
+
+            if (!String.IsNullOrWhiteSpace(ghostUsername) && !String.IsNullOrWhiteSpace(ghostPassword))
+            {
+                if (accessToken == null)
+                {
+                    var authRequest = new RestRequest("authentication/token", Method.POST);
+
+                    authRequest.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+                    authRequest.AddHeader("Accept", "application/json");
+
+                    authRequest.AddParameter("grant_type", "password");
+                    authRequest.AddParameter("client_id", ghostClientId);
+                    authRequest.AddParameter("client_secret", ghostClientSecret);
+                    authRequest.AddParameter("username", ghostUsername);
+                    authRequest.AddParameter("password", ghostPassword);
+
+                    var client = new RestClient { BaseUrl = new Uri($"{ghostUri}{API_URL}") };
+
+                    accessToken = client.Execute<AuthToken>(authRequest).Data.AccessToken;
+                }
+
+                request.AddParameter("Authorization", $"Bearer {accessToken}", ParameterType.HttpHeader);
+            }
+            else
+            {
+                request.AddQueryParameter("client_id", ghostClientId);
+                request.AddQueryParameter("client_secret", ghostClientSecret);
+            }
+        }
+    }
+}
